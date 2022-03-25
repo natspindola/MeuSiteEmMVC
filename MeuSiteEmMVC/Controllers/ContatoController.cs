8f0a80a79d75f910e92ec55e84bc6438e43476c0
natspindola <natspindola@hotmail.com>
2022-03-25 19:20:10 -0300

    criando views

--- MeuSiteEmMVC/Controllers/ContatoController.cs
+++ MeuSiteEmMVC/Controllers/ContatoController.cs
@@ -12,5 +12,20 @@ namespace MeuSiteEmMVC.Controllers
         {
             return View();
         }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
     }
 }
