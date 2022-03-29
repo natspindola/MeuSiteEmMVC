2976b44c6439b6a1137b8a7d5f90a2ec073d059c
natspindola <natspindola@hotmail.com>
2022-03-28 21:01:36 -0300

    construtor BD

--- MeuSiteEmMVC/Data/BancoContext.cs
+++ MeuSiteEmMVC/Data/BancoContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using MeuSiteEmMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MeuSiteEmMVC.Data
 {
-    public class BancoContext
+    public class BancoContext : DbContext
     {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
     }
 }
