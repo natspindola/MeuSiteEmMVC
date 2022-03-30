37fa7de74d0489979fb62ca4989c3c95c0ed7299
natspindola <natspindola@hotmail.com>
2022-03-30 18:02:15 -0300

    required

--- MeuSiteEmMVC/Models/ContatoModel.cs
+++ MeuSiteEmMVC/Models/ContatoModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,16 @@ namespace MeuSiteEmMVC.Models
     public class ContatoModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do contato")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o email do contato")]
+        [EmailAddress(ErrorMessage = "O email informado não é válido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Digite o telefone do contato")]
+        [EmailAddress(ErrorMessage = "O telefone informado não é válido")]
         public string Telefone { get; set; }
     }
 }
