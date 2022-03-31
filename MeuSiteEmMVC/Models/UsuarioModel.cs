d3868976d9df12f12eb9da7a0889458f396bb186
natspindola <natspindola@hotmail.com>
2022-03-31 18:46:50 -0300

    usuario model

---
+++ MeuSiteEmMVC/Models/UsuarioModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuSiteEmMVC.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
