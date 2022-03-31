0d3e9fe4d0305a2be54bb5219d7ca0e34c63de52
natspindola <natspindola@hotmail.com>
2022-03-31 19:32:28 -0300

    repositorio usuario

---
+++ MeuSiteEmMVC/Repositorio/IUsuarioRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using MeuSiteEmMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuSiteEmMVC.Repositorio
+{
+    public interface IUsuarioRepositorio
+    {
+        ContatoModel ListarPorId(int id);
+        List<UsuarioModel> BuscarTodos();
+        UsuarioModel Adicionar(UsuarioModel usuario);
+        UsuarioModel Atualizar(UsuarioModel usuario);
+        bool Apagar(int id);
+    }
+}
